abe63dccc1f0975274476ef64fc8eecd34517bfe
Carter Chan <chanc28@up.edu>
2026-02-05 18:23:04 -0800

    added main page

---
+++ hw3/public/javascripts/CHANC28.js
@@ -0,0 +1,44 @@
+// Made by: Carter Chan
+$(document).ready(function() {
+	$("#placeOrder").click(function() {
+		let orderDetails = "";
+		let toppingString = "";
+		
+		// should get the selected topping
+		if (document.getElementById('blueberry').checked) {
+			toppingString = "Blueberry"
+		}
+		else if (document.getElementById('strawberry').checked) {
+			toppingString = "Strawberry"
+		}
+		else if (document.getElementById('wildberry').checked) {
+			toppingString = "Wildberry"
+		}
+		else {
+			toppingString = "Plain"
+		}
+		
+		// should get the selected number of cheesecakes
+		var dropdown = document.getElementById('quantityOfCheesecakes');
+		var numberOfCheesecakes = dropdown.value;
+		
+		// checks if the word vegan is written in notes
+		var textarea = document.getElementById('notes');
+		var vegan = "vegan";
+		var comments = textarea.value.toLowerCase();
+		
+		// checks if the word vegan is in the notes section
+		// and asks for confirmation
+		if (comments.includes(vegan)) {
+			if (!confirm("Cheesecake contains dairy. Do you wish to order?")) {
+				return;
+			}
+		}
+		
+		orderDetails = "Ordered: " + numberOfCheesecakes + " " + toppingString + " Cheesecake(s)";
+			
+		
+		$("#orderInfo").replaceWith("<h2>Thank you for placing your order!</h2><p>" + orderDetails + "</p>");
+		
+	})
+});
